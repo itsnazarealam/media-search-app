d800ae3b64f70c04214e7bf5daf6f4a89ec6f36e
Md Nazare Alam <79821343+itsnazarealam@users.noreply.github.com>
2026-06-22 19:46:52 +0530

    Replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-import {BrowserRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
+      {/* </BrowserRouter> */}
     </Provider>
   </React.StrictMode>,
 )
